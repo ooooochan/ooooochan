43ec4cbf7488f494ebe131f9a2e1f4fd4f263fe9
hamsu125 <144293452+hamsu125@users.noreply.github.com>
2023-09-07 16:07:44 +0900

    ct

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-	printf("Hello Hello\n");
+	int input, input2;
+	scanf_s("%d", &input);
+	input2 = (input * 2) - 1;
+	for (int i = 1; i <= input; i++) {
+		for (int j = 1; j <= (2*i-1); j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
