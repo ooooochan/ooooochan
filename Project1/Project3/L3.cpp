b5642b8fb636adf6bd1f5376d0f14c2fef3ebde4
ooooochan <144310775+ooooochan@users.noreply.github.com>
2023-10-12 15:23:05 +0900

    score2

--- Project1/Project3/L3.cpp
+++ Project1/Project3/L3.cpp
@@ -4,11 +4,14 @@ int main(void)
 {
 	int score[5] = { 87, 92, 89, 98, 78 };
 	int cnt;
+	int* p_score = &score[1];
 
 	printf("score[5] = { 87, 92, 89, 98, 78 } 배열 요소 출력\n");
 
-	for (cnt = 0; cnt < 5; cnt++)
-		printf("score[%d]=[%d]\n", cnt, score[cnt]);
+	for (cnt = 0; cnt < 3; cnt++) {
+		printf("%d\n", *p_score);
+		p_score++;
+	}
 
 	return 0;
 }
